1bb53087690ac7fa6834ceeaeada9bfb084bb169
Darren Wei <wwdarrenwei@gmail.com>
2025-10-12 17:32:53 -0600

    Disable install button during installation

---
+++ src/Lacesong.Avalonia/Converters/InverseIdMatchConverter.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using System.Collections.Generic;
+
+namespace Lacesong.Avalonia.Converters;
+
+public class InverseIdMatchConverter : IMultiValueConverter
+{
+    public object Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (values.Count < 2) return true;
+        var modId = values[0]?.ToString();
+        var installingId = values[1]?.ToString();
+        // return true (enabled) when NOT installing this mod
+        return string.IsNullOrEmpty(installingId) || installingId != modId;
+    }
+}
+
